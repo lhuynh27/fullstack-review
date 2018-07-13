3ae538870cbedf54a8a68e53f508c7e3a77249eb
lhuynh27 <leannehuynh27@gmail.com>
2018-07-13 14:03:10 -0700

    Update schema

--- database/index.js
+++ database/index.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  repo_id: Number,
+  username: String,
+  repo_name: String,
+  repo_url: String,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+  // This function should save a repo or repos to the MongoDB
+  
 }
 
 module.exports.save = save;
