2a53ded2581b3daafbb1689bd59a75b292ab2d57
lhuynh27 <leannehuynh27@gmail.com>
2018-07-14 08:57:26 -0700

    Remove body parser dependency

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 let app = express();
-const getReposByUsername = require('../helpers/github.js').getReposByUsername
-const bodyParser = require('body-parser')
+const getReposByUsername = require('../helpers/github.js').getReposByUsername;
+const save = require('../database/index.js').save;
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-// app.use(bodyParser.urlencoded({ extended: false }))
 
+// This route should take the github username provided
+// and get the repo information from the github API, then
+// save the repo information in the database
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
   let body = '';
   req.on('data',(chunk) => {
     body += chunk;
   }).on('end', () => {
     getReposByUsername(body, (data) => {
       //SEND OVER TO DATABASE
+      //USE SAVE FUNCTION HERE
+
       res.send('Post Works!') 
     });
   });
