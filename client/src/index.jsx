59286ffb82a6435b872aec7a040ae2e66da218c3
lhuynh27 <leannehuynh27@gmail.com>
2018-07-14 15:31:04 -0700

    Add top 25 to page via componentDidMount

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -13,10 +13,24 @@ class App extends React.Component {
   this.search = this.search.bind(this);
   }
 
+  componentDidMount() {
+    let contextScope = this;
+    $.get("http://127.0.0.1:1128/repos")
+      .done((data) => {
+        contextScope.setState({
+          repos: data
+        })
+      });
+  }
+
   search (term) {
+    let contextScope = this;
     console.log(`${term} was searched`);
     $.post("http://127.0.0.1:1128/repos", term)
       .done( (data)=> {
+        contextScope.setState({
+          repos: data
+        })
         console.log('AJAX Request Successful!');
     });
   }
