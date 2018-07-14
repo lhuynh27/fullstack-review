ce994ed87118b080c7f64cdef34f53cfea865ba3
lhuynh27 <leannehuynh27@gmail.com>
2018-07-14 15:30:23 -0700

    Add items

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Item from './Item.jsx';
 
-const RepoList = (props) => (
-  <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
-  </div>
-)
+const RepoList = (props) => {
+	return(
+  		<div>
+		    <h4> Repo List Component </h4>
+		    There are {props.repos.length} repos.
+		    {props.repos.map((repo) => {
+		    	return(<Item repo={repo} key={repo.repo_id} />)
+		    	})
+    	}	
+  		</div>
+	)
+};
 
 export default RepoList;
