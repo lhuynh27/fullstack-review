86b72c8d9a97367d816f21978d687b18002e229b
lhuynh27 <leannehuynh27@gmail.com>
2018-07-13 20:22:30 -0700

    Update app.post

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 let app = express();
+const getReposByUsername = require('../helpers/github.js').getReposByUsername
+const bodyParser = require('body-parser')
 
 app.use(express.static(__dirname + '/../client/dist'));
+
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  let body = '';
+  req.on('data',(chunk) => {
+    body += chunk;
+  }).on('end', () => {
+    getReposByUsername(body, (data) => {
+      //SEND OVER TO DATABASE
+      res.send('Post Works!') 
+    });
+  });
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  res.send('Get Works')
 });
 
 let port = 1128;
